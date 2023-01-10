ce8b67b438c3281a9fd8dd7f66bb9daba2d28237
이상호 <kigkm@naver.com>
2023-01-11 01:35:24 +0900

    update

--- frontend/src/component/App.tsx
+++ frontend/src/component/App.tsx
@@ -1,167 +1,139 @@
-import React, { useEffect, useState, useRef, useCallback } from "react";
-import axios, { AxiosResponse } from "axios";
-import { AiOutlineBell, AiOutlineHome } from "react-icons/ai";
-import { MdOutlineMessage } from "react-icons/md";
-import { Outlet } from "react-router-dom";
-import Footer from "../page/Footer";
-import Header from "../page/Header";
+import React, { useEffect, useState, useRef, useCallback } from 'react';
+import axios, { AxiosResponse } from 'axios';
+import { AiOutlineBell, AiOutlineHome } from 'react-icons/ai';
+import { MdOutlineMessage } from 'react-icons/md';
+import { Outlet } from 'react-router-dom';
+import Footer from '../page/Footer';
+import Header from '../page/Header';
 
 // import Content from "../component/Content";
 
 export interface playlist {
-    etag?: string;
-    items?: Snippet[];
-    kind?: string;
-    nextPageToken?: string;
-    pageInfo?: {
-        resultsPerPage: string;
-        totalResults: string;
-    };
+  etag?: string;
+  items?: Snippet[];
+  kind?: string;
+  nextPageToken?: string;
+  pageInfo?: {
+    resultsPerPage: string;
+    totalResults: string;
+  };
 }
 
 export interface Snippet {
-    kind: string;
-    etag: string;
-    id: string;
-    contentDetails: {
-        itemCount: string;
-    };
+  kind: string;
+  etag: string;
+  id: string;
+  contentDetails: {
+    itemCount: string;
+  };
 
-    snippet: {
-        channelId: string;
-        channelTitle: string;
-        title: string;
-        description: string;
-        localized: {
-            title: string;
-            description: string;
-        };
-        thumbnails: {
-            [key: string]: {
-                [key: string]: string;
-            };
-        };
+  snippet: {
+    channelId: string;
+    channelTitle: string;
+    title: string;
+    description: string;
+    localized: {
+      title: string;
+      description: string;
     };
+    thumbnails: {
+      [key: string]: {
+        [key: string]: string;
+      };
+    };
+  };
 
-    [key: string]: String | Object;
+  [key: string]: String | Object;
 }
 
 const App = () => {
-    const loging = useRef<any>(null);
-    const [maxResults, setMaxResults] = useState(10);
-    const [page, setPage] = useState(1);
-    const [playlists, setPlaylists] = useState<playlist>({
-        etag: "",
-        items: [],
-        kind: "",
-        nextPageToken: "",
-        pageInfo: {
-            resultsPerPage: "",
-            totalResults: "",
-        },
-    });
-    const intersectHandler = useCallback(([entry]: any) => {
-        if (entry.isIntersecting) {
-            setPage((page) => page + 1);
-        }
-    }, []);
+  const loging = useRef<any>(null);
+  const [maxResults, setMaxResults] = useState(10);
+  const [page, setPage] = useState(1);
+  const [playlists, setPlaylists] = useState<playlist>({
+    etag: '',
+    items: [],
+    kind: '',
+    nextPageToken: '',
+    pageInfo: {
+      resultsPerPage: '',
+      totalResults: '',
+    },
+  });
+  const intersectHandler = useCallback(([entry]: any) => {
+    if (entry.isIntersecting) {
+      setPage((page) => page + 1);
+    }
+  }, []);
 
-    useEffect(() => {
-        const getData = async () => {
-            try {
-                const value = await axios.get<playlist>(
-                    `playlists?maxResults=${maxResults}&nextPageToken=${playlists.nextPageToken}`
-                );
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const value = await axios.get<playlist>(
+          `playlists?maxResults=${maxResults}&nextPageToken=${playlists.nextPageToken}`
+        );
 
-                setPlaylists((prev: any) => {
-                    let data: any = [];
+        setPlaylists((prev: any) => {
+          let data: any = [];
 
-                    // console.log(prev);
-                    // console.log(value.data);
-                    if (value.data.items) {
-                        value.data.items.forEach((el: Snippet) => {
-                            data.push(el.snippet);
-                        });
-                        const newObject = {
-                            ...value.data,
-                            items: [...prev.items, ...value.data.items],
-                        };
+          // console.log(prev);
+          // console.log(value.data);
+          if (value.data.items) {
+            value.data.items.forEach((el: Snippet) => {
+              data.push(el.snippet);
+            });
+            const newObject = {
+              ...value.data,
+              items: [...prev.items, ...value.data.items],
+            };
 
-                        return newObject;
-                    } else {
-                        return prev;
-                    }
+            return newObject;
+          } else {
+            return prev;
+          }
 
-                    // value.data.item: total
-                });
-            } catch (error) {
-                if (error) {
-                    console.error(error);
-                }
-            }
-        };
-        getData();
-    }, [page]);
-
-    useEffect(() => {
-        const observer = new IntersectionObserver(intersectHandler, {
-            threshold: 0,
+          // value.data.item: total
         });
+      } catch (error) {
+        if (error) {
+          console.error(error);
+        }
+      }
+    };
+    getData();
+  }, [page]);
 
-        observer.observe(loging.current);
-        return () => observer.disconnect();
-    }, [intersectHandler]);
+  useEffect(() => {
+    const observer = new IntersectionObserver(intersectHandler, {
+      threshold: 0,
+    });
 
-    return (
-        <>
-            <div className="bg-gray-100 flex flex-col items-center justify-center">
-                <div className="flex flex-col bg-white">
-                    {playlists.items?.length !== 0 && (
-                        <>
-                            <Header />
-                            <Outlet context={{ playlists }} />
-                            <Footer />
-                        </>
-                    )}
-                    {/* <div className="flex flex-col items-center p-4"> */}
-                    {/* {playlists.items?.map((el: Snippet, i: number) => {
-                            return (
-                                <div className="w-[320px]" key={i}>
-                                    <div
-                                        className="cursor-pointer"
-                                        onClick={() => {
-                                            setColor("text-amber-400");
-                                        }}
-                                    >
-                                        <img
-                                            className="min-w-full"
-                                            src={`${el.snippet.thumbnails.medium.url}`}
-                                            alt="이미지"
-                                        />
-                                        <p className={color}>
-                                            {el.snippet.title}
-                                        </p>
-                                    </div>
+    observer.observe(loging.current);
+    return () => observer.disconnect();
+  }, [intersectHandler]);
 
-                                    <p>
-                                        {el.contentDetails.itemCount} 영상 개수
-                                    </p>
-                                </div>
-                            );
-                        })} */}
+  return (
+    <>
+      <div className='bg-gray-100 flex flex-col items-center justify-center'>
+        <div className='flex flex-col bg-white'>
+          {playlists.items?.length !== 0 && (
+            <>
+              <Header />
+              <Outlet context={{ playlists }} />
+              <Footer />
+            </>
+          )}
 
-                    <div
-                        className={
-                            playlists.items?.length === 0 ? "hidden" : "block"
-                        }
-                        ref={loging}
-                    >
-                        로딩 중 입니다.
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+          <div
+            className={playlists.items?.length === 0 ? 'hidden' : 'block'}
+            ref={loging}
+          >
+            로딩 중 입니다.
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
